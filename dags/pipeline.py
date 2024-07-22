442a3dffb2f5e14bd5a975ff6bc1b939c0ed4740
Fasai Serikijcharoen <fasai.seri@gmail.com>
2024-07-22 14:11:01 +0700

    add merge_booking_flight_and_clean

--- dags/pipeline.py
+++ dags/pipeline.py
@@ -102,7 +102,20 @@ def get_customer_data_from_gcs(output_path):
     df.to_parquet(output_path, index=False)
     print(f'output to {output_path}')
     
-
+@task()
+def merge_booking_flight_and_clean(booking_data_path, flight_data_path):
+    # read data
+    booking = pd.read_parquet(booking_data_path)
+    flight = pd.read_parquet(flight_data_path)
+    
+    # merge data
+    df = pd.merge(booking, flight, how='inner', on='flight_id')
+    
+    # remove records with booking timestamp more than departure timestamp
+    df = df[df['booking_timestamp'] < df['departing_timestamp']]
+    
+    
+    
     
 # @task()
 # booking timestamp < departure timestamp
@@ -110,7 +123,7 @@ def get_customer_data_from_gcs(output_path):
 @dag(default_args=default_args, schedule_interval='@once', start_date=days_ago(1), tags=['practice'])
 def flight_data_pipeline():
     """
-    load all data into BigQuery
+    Extradt datasets from various sources and load into BigQuery
     """
     
     t1 = get_airport_data_from_gcs(output_path=airport_data_output_path)
@@ -118,5 +131,10 @@ def flight_data_pipeline():
     t3 = get_country_data_from_website(output_path=country_data_output_path)
     t4 = get_customer_data_from_gcs(output_path=customer_data_output_path)
     t5 = get_flight_data_from_gcs(output_path=flight_data_output_path)
+    t6 = merge_booking_flight_and_clean(booking_data_path=booking_data_output_path, flight_data_path=flight_data_output_path)
+    
+    [t2, t5] >> t6
+    
+    
     
     
